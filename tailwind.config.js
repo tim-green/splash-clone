9fdb680bf38a68005ea5adab132984de51d7418c
Tim <github@timgreen.ws>
2019-09-08 22:05:05 +1000

    added font family to theme config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   theme: {
-    extend: {}
+        extend: {
+         fontFamily: {
+         'sans': ['-apple-system', 'BlinkMacSystemFont'],
+         'serif': ['Georgia', 'Cambria'],
+         'mono': ['SFMono-Regular', 'Menlo'],
+         'display': ['Oswald'],
+         'body': ['Open Sans'],
+        }
+    }
   },
   variants: {},
   plugins: []
